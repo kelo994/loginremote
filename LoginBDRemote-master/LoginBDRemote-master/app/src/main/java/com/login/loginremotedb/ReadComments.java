212027321ee39632b4dd1fcba830fd985eca949b
kelo994 <eze.monsal@gmail.com>
2016-05-19 13:50:54 -0300

    commit

---
+++ LoginBDRemote-master/LoginBDRemote-master/app/src/main/java/com/login/loginremotedb/ReadComments.java
@@ -0,0 +1,18 @@
+package com.login.loginremotedb;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.login.loginbdremote.R;
+
+/**
+ * Created by Ramiro on 25/01/2015.
+ */
+public class ReadComments extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // note that use read_comments.xml instead of our single_post.xml
+        setContentView(R.layout.read_comments);
+    }
+}
